9c2bd40d04461aae26fc2f82c09aa85e693e10d3
antoniobigas <antoniobigas@gmail.com>
2022-12-15 19:23:01 -0300

    Many-to-Many e Association JoinTable

--- course/src/main/java/com/educandonaweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandonaweb/course/config/TestConfig.java
@@ -33,6 +33,8 @@ public class TestConfig  implements CommandLineRunner {
     private OrderRepository orderRepository;
 
     public void run(String... args) throws Exception {
+
+
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
@@ -46,6 +48,16 @@ public class TestConfig  implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -61,3 +73,4 @@ public class TestConfig  implements CommandLineRunner {
 
     }
 }
+
